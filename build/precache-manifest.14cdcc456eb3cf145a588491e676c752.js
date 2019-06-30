3d642fd50b378d6c84831aabd07d23bba8c2f2fe
Kevin Lee <kevinlee963@gmail.com>
2019-06-29 19:57:58 -0700

    Add build files

---
+++ build/precache-manifest.14cdcc456eb3cf145a588491e676c752.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ba1dd39334d38adbd23faba1c6d37d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b60e9708ce5f6454744d",
+    "url": "/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "b60e9708ce5f6454744d",
+    "url": "/static/js/2.3ca0c576.chunk.js"
+  },
+  {
+    "revision": "d1902793977ffc64f6c5",
+    "url": "/static/js/main.370cd69b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
